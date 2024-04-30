9a7cb3bd47234559deb494fff8a81a301b1f0ed7
QSnake_macbookair <jester36883688@gmail.com>
2024-04-30 19:34:37 +0800

    resovle some bug in og generator

--- scripts/pre-build/generateOGImage.ts
+++ scripts/pre-build/generateOGImage.ts
@@ -134,7 +134,14 @@ function drawTag(ctx: CanvasRenderingContext2D, tag: string, x: number, y: numbe
 
 
 async function generateSessionImage(session: Session,typeById: SomethingById,nameById: SomethingById): Promise<void> {
-  const outputPath = path.resolve(__dirname, '../../public/images/sessions', `${session.id}.png`);
+  const dirPath = path.resolve(__dirname, '../../public/images/sessions');
+  // Check if the directory exists, if not, create it
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+    console.log(`Directory created at ${dirPath}`);
+  }
+
+  const outputPath = path.join(dirPath, `${session.id}.png`);
   // check if the picture exists
   if (fs.existsSync(outputPath)) {
     // console.log(`Image for session ${session.id} already exists at ${outputPath}. Skipping generation.`);
