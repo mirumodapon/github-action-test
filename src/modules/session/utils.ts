d0be56d0f14f79db156d26b3c28ee0e4c6128880
QSnake_macbookair <jester36883688@gmail.com>
2024-07-14 11:03:26 +0800

    add docstring in module:session utils.ts

--- src/modules/session/utils.ts
+++ src/modules/session/utils.ts
@@ -15,6 +15,11 @@ export function fixedTimeZoneDate (date: Date | string, timeZoneOffsetMinutes: n
   return date
 }
 
+/**
+ * Extract the components of a Date object
+ * @param date The Date object to extract detail from.
+ * @returns An object containing components of the date(year, month, date, day, hour, minutes).
+ */
 export function getPartsOfDate (date: Date): PartsOfDate {
   return {
     year: date.getFullYear(),
@@ -26,16 +31,33 @@ export function getPartsOfDate (date: Date): PartsOfDate {
   }
 }
 
+/**
+ * Pads a number with a leading zero if it has less than 2 digits.
+ * @param number The number to pad.
+ * @returns The padded number as a string.
+ */
 export function padNumberStart2WithZero (number: number) {
   return number.toString().padStart(2, '0')
 }
 
+/**
+ * Formats a Data object into a string containing the year, month, date, with optional padding.
+ * @param dataObj The Date object to formate.
+ * @param joinChar The character to join the year, month, and date. Default is an empty string.
+ * @returns The formatted date string.
+ */
 export function formatDateString (dataObj: Date, joinChar = '') {
   const { year, month, date } = getPartsOfDate(dataObj)
   return [year, month, date]
     .map(padNumberStart2WithZero).join(joinChar)
 }
 
+/**
+ * Format Data object into a string containing the hour, minute, with optional padding.
+ * @param dateObj The Date object to formate.
+ * @param joinChar The character to joint the hour and minute. Default is an empty string.
+ * @returns The formated time string.
+ */
 export function formatTimeString (dateObj: Date, joinChar = '') {
   const { hour, minute } = getPartsOfDate(dateObj)
   return [hour, minute]
